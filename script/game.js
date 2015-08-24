ceef4e54d4566a99af43e607a702d6dfebb1a13f
Logan Franken <loganfranken@gmail.com>
2015-08-23 21:29:22 -0700

    The reverse of the user's line is now drawn

--- script/game.js
+++ script/game.js
@@ -16,14 +16,25 @@ Game.prototype.update = function()
 
 Game.prototype.draw = function()
 {
+  var self = this;
   var context = this.context;
 
   // Clear the canvas
   context.fillStyle = '#FFF';
   context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
 
+  var halfCanvasHeight = this.canvasHeight/2;
+
+  // Draw the top half
+  context.fillStyle = '#FFF';
+  context.fillRect(0, 0, this.canvasWidth, halfCanvasHeight);
+
+  // Draw the bottom half
+  context.fillStyle = '#333';
+  context.fillRect(0, halfCanvasHeight, this.canvasWidth, halfCanvasHeight);
+
   // Draw the user's line
-  context.fillStyle = '#000';
+  context.strokeStyle = '#000';
   context.beginPath();
   context.lineWidth = 5;
 
@@ -36,6 +47,30 @@ Game.prototype.draw = function()
     else
     {
       context.lineTo(point.x, point.y);
+    }
+
+  });
+
+  context.lineJoin = 'round';
+  context.stroke();
+
+  // Draw the reverse of the user's line
+  context.strokeStyle = '#FFF';
+  context.beginPath();
+  context.lineWidth = 5;
+
+  this.linePoints.forEach(function(point, i) {
+
+    var x = self.canvasWidth - point.x;
+    var y = self.canvasHeight - point.y;
+
+    if(i === 0)
+    {
+      context.moveTo(x, y);
+    }
+    else
+    {
+      context.lineTo(x, y);
     }
 
   });
@@ -75,6 +110,11 @@ Game.prototype.handleMouseDown = function(game, mouseEvent)
 
 Game.prototype.handleMouseMove = function(game, mouseEvent)
 {
+  if(mouseEvent.clientY > (game.canvasHeight/2))
+  {
+    game.linePoints = [];
+  }
+
   if(game.isMouseDown)
   {
     game.linePoints.push({ x: mouseEvent.clientX, y: mouseEvent.clientY });
