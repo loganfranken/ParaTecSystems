cca37f2079fca7d5414e0f820f4c62db5af33d0d
Logan Franken <loganfranken@gmail.com>
2015-08-23 21:13:06 -0700

    Basic drawing mechanic is working

--- script/game.js
+++ script/game.js
@@ -3,6 +3,9 @@ function Game(canvas)
   this.canvas = canvas;
   this.context = canvas.getContext("2d");
 
+  this.isMouseDown = false;
+  this.linePoints = [];
+
   var canvasWidth = canvas.width;
   var canvasHeight = canvas.height;
 }
@@ -13,13 +16,34 @@ Game.prototype.update = function()
 
 Game.prototype.draw = function()
 {
+  var context = this.context;
+
+  context.beginPath();
+  context.lineWidth = 5;
+
+  this.linePoints.forEach(function(point, i) {
+
+    if(i === 0)
+    {
+      context.moveTo(point.x, point.y);
+    }
+    else
+    {
+      context.lineTo(point.x, point.y);
+    }
+
+  });
+
+  context.lineJoin = 'round';
+  context.stroke();
 }
 
 Game.prototype.start = function()
 {
   var self = this;
 
-  self.canvas.addEventListener('click', function(mouseEvent) { self.handleClick(self, mouseEvent) }, false);
+  self.canvas.addEventListener('mousedown', function(mouseEvent) { self.handleMouseDown(self, mouseEvent) }, false);
+  self.canvas.addEventListener('mouseup', function(mouseEvent) { self.handleMouseUp(self, mouseEvent) }, false);
   self.canvas.addEventListener('mousemove', function(mouseEvent) { self.handleMouseMove(self, mouseEvent) }, false);
 
   function loop()
@@ -28,14 +52,25 @@ Game.prototype.start = function()
     self.draw();
   }
 
-  window.setInterval(loop, 100);
+  window.setInterval(loop, 50);
   loop();
 }
 
-Game.prototype.handleClick = function(game, mouseEvent)
+Game.prototype.handleMouseUp = function(game, mouseEvent)
 {
+  game.isMouseDown = false;
+}
+
+Game.prototype.handleMouseDown = function(game, mouseEvent)
+{
+  game.isMouseDown = true;
 }
 
 Game.prototype.handleMouseMove = function(game, mouseEvent)
 {
+  if(game.isMouseDown)
+  {
+    console.log(mouseEvent);
+    game.linePoints.push({ x: mouseEvent.clientX, y: mouseEvent.clientY });
+  }
 }
---
+++ todo.md
@@ -0,0 +1,4 @@
+# TODO
+
+* Erase the line after the user lifts their mouse
+* Mirror the drawing of that line
