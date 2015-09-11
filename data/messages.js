dfff307c62d944f2b429027268a3cdffdea4e217
Logan Franken <loganfranken@gmail.com>
2015-09-11 00:06:31 -0700

    Adjusted messaging

--- data/messages.js
+++ data/messages.js
@@ -50,24 +50,24 @@ messages[1][3] = [
   { speaker: 'Otto', content: "Hey, how are you doing?" },
   { speaker: 'Otto', content: "Hold 'Reply' to respond", delay: 10 },
   { speaker: 'Otto', content: "(If you want)", delay: 10, awaitReply: true, replyDelay: 100, replyMessage: { speaker: 'You', content: "Doing just fine" } },
-  { speaker: 'Otto', content: "Hey, glad to hear it", delay: 10, condition: function(game) { return game.repliedToLastMessage; } },
-  { speaker: 'Otto', content: "Alright, I'll leave you to it", delay: 10, condition: function(game) { return !game.repliedToLastMessage; } },
+  { speaker: 'Otto', content: "Hey, glad to hear it", delay: 20, condition: function(game) { return game.repliedToLastMessage; } },
+  { speaker: 'Otto', content: "Alright, I'll leave you to it", delay: 20, condition: function(game) { return !game.repliedToLastMessage; } },
 ];
 
 /* Day 3, Stage 0 */
 messages[2][0] = [
   { speaker: 'Otto', content: "We're turning on scoring for you now" },
-  { speaker: 'Otto', content: "The faster you go, the higher your score", delay: 10 },
-  { speaker: 'Otto', content: "Don't let it freak you out, though, okay?", delay: 10 },
-  { speaker: 'Otto', content: "You're doing just fine", delay: 10 }
+  { speaker: 'Otto', content: "The faster you go, the higher your score", delay: 20 },
+  { speaker: 'Otto', content: "Don't let it freak you out, though, okay?", delay: 20 },
+  { speaker: 'Otto', content: "You're doing just fine", delay: 20 }
 ];
 
 /* Day 3, Stage 3 */
 messages[2][3] = [
   { speaker: 'Otto', content: "Be honest" },
-  { speaker: 'Otto', content: "Is the score kind of freaking you out?", delay: 10, awaitReply: true, replyDelay: 70, replyMessage: { speaker: 'You', content: "It's alright" } },
-  { speaker: 'Otto', content: "Dang, you're a champ", delay: 10, condition: function(game) { return game.repliedToLastMessage; } },
-  { speaker: 'Otto', content: "Well, don't let it freak you out if it is", delay: 10, condition: function(game) { return !game.repliedToLastMessage; } }
+  { speaker: 'Otto', content: "Is the score kind of freaking you out?", delay: 20, awaitReply: true, replyDelay: 70, replyMessage: { speaker: 'You', content: "It's alright" } },
+  { speaker: 'Otto', content: "Dang, you're a champ", delay: 20, condition: function(game) { return game.repliedToLastMessage; } },
+  { speaker: 'Otto', content: "Well, don't let it freak you out if it is", delay: 20, condition: function(game) { return !game.repliedToLastMessage; } }
 ];
 
 /* Day 3, Stage 5 */
@@ -79,9 +79,9 @@ messages[2][5] = [
 /* Day 4, Stage 3 */
 messages[3][3] = [
   { speaker: 'Otto', content: "Hey, I know this is tough work" },
-  { speaker: 'Otto', content: "But you're doing really well", delay: 10, awaitReply: true, replyDelay: 50, replyMessage: { speaker: 'You', content: "Thanks for everything, Otto" } },
-  { speaker: 'Otto', content: "Aww, no problem", delay: 10, condition: function(game) { return game.repliedToLastMessage; } },
-  { speaker: 'Otto', content: "You'll do great here", delay: 10, condition: function(game) { return !game.repliedToLastMessage; } }
+  { speaker: 'Otto', content: "But you're doing really well", delay: 20, awaitReply: true, replyDelay: 50, replyMessage: { speaker: 'You', content: "Thanks for everything, Otto" } },
+  { speaker: 'Otto', content: "Aww, no problem", delay: 20, condition: function(game) { return game.repliedToLastMessage; } },
+  { speaker: 'Otto', content: "You'll do great here", delay: 20, condition: function(game) { return !game.repliedToLastMessage; } }
 ];
 
 /* Day 4, Stage 5 */
@@ -93,7 +93,7 @@ messages[3][5] = [
 /* Day 5, Stage 0 */
 messages[4][0] = [
   { speaker: 'Otto', content: "Last day of your first week!" },
-  { speaker: 'Otto', content: "Come grab a beer with us after work tonight", delay: 10, condition: function(game) { return game.replyCount > 2; } }
+  { speaker: 'Otto', content: "Come grab a beer with us after work tonight", delay: 20, condition: function(game) { return game.replyCount > 2; } }
 ];
 
 /* Day 5, Stage 5 */
